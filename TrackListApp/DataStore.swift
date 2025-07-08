0b1da2b47372906788dee479e5270cd4e6648bd9
Kann96 <kann9620@gmail.com>
2025-07-08 21:04:38 +0300

    mur

---
+++ TrackListApp/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  TrackListApp
+//
+//  Created by Анастасия Кучер on 01.07.2025.
+//
+
+import Foundation
---
+++ TrackListApp/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  TrackListApp
+//
+//  Created by Анастасия Кучер on 01.07.2025.
+//
+
+import Foundation
