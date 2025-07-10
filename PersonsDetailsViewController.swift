afe4c2322b0616ecf3d2dd19bb34c5ea6da32b77
Kann96 <kann9620@gmail.com>
2025-07-10 19:21:10 +0300

    details

---
+++ PersonsDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonsDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Анастасия Кучер on 10.07.2025.
+//
+
+import UIKit
+
+class PersonsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TrackListApp/PersonsDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PersonsDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Анастасия Кучер on 10.07.2025.
+//
+
+import UIKit
+
+final class PersonsDetailsViewController: UIViewController {
+
+    @IBOutlet var fullNameLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
